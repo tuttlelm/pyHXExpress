90dae41592622e9b2de1e27967a17a2e71dd4c6c
tuttlelm <113445998+tuttlelm@users.noreply.github.com>
2024-04-12 13:20:14 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.100'
+DESCRIPTION = 'pyHXExpress'
+LONG_DESCRIPTION = 'High-throughput polymodal analysis of protein HDX-MS spectra'
+
+setup(
+    name="pyhxexpress",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="<Lisa M Tuttle>",
+    author_email="<tuttlelm@uw.edu>",
+    url='https://github.com/tuttlelm/pyHXExpress',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=['numpy','pandas','importlib','pymupdf','scipy','matplotlib','pyteomics',
+              'brain-isotopic-distribution','biopython','tensorflow==2.13.0','keras'],
+    keywords='HDX-MS',
+
+    py_modules = ['pyhxexpress']
+    
+  
+    classifiers= [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        "Programming Language :: Python :: 3",
+    ]
+)
