2d142f3986db26725e2b962cd86488f5a15bdab4
Lisa M Tuttle <113445998+tuttlelm@users.noreply.github.com>
2025-07-24 09:24:38 -0700

    added C/Nterm mods functionality

--- pyhxexpress/hxex_fragments.py
+++ pyhxexpress/hxex_fragments.py
@@ -5,6 +5,7 @@ pyteomics std_ion_comp
 https://github.com/levitsky/pyteomics/blob/master/pyteomics/mass/mass.py
 '''
 
+from pyteomics import parser
 import pandas as pd
 import numpy as np
 
@@ -47,7 +48,7 @@ def get_fragments(metadf,user_frags=None):
     specify as 'ion_type:fragments:charges' where :charges is optional
     e.g. 'b-NH3:1,2,7 x:ALL y:1,2:1,2'
      '''
-    def get_pepfrag(trunc_type):
+    def get_pepfrag(peptide,trunc_type,trunc):
         if (trunc_type == 'N-term'):
             start_idx = 0
             end_idx = trunc
@@ -58,6 +59,22 @@ def get_fragments(metadf,user_frags=None):
             start_idx = 0
             end_idx = len(peptide)
         return start_idx,end_idx
+    
+    def expand_range(values,start,end):
+        expanded_list = []
+        if 'ALL' in values:
+            expanded_list += list(np.arange(int(start),int(end)+1))
+        else:
+            for itrunc in values:
+                if len(itrunc.split('-'))>1:
+                    ltrunc = itrunc.split('-')[0]               
+                    rtrunc = itrunc.split('-')[1]
+                    ltrunc = int(ltrunc) if len(ltrunc) > 0 else int(start)
+                    rtrunc = int(rtrunc) if len(rtrunc) > 0 else int(end)
+                    expanded_list += list(np.arange(ltrunc,rtrunc+1))
+                else:
+                    expanded_list += [int(itrunc)]
+        return set(expanded_list)
 
     frags_df = pd.DataFrame()
 
@@ -68,7 +85,25 @@ def get_fragments(metadf,user_frags=None):
             new_mods = ' '.join(list(filter(lambda x: x not in it,mods)))+' '
 
         else: new_mods = ''
-        peptide = row.peptide
+        sequence = row.peptide
+        
+        pep_parts = parser.parse(sequence)
+        if '-' in pep_parts[0]:
+            pep_nterm = pep_parts[0]
+            pep_start = 1
+        else: 
+            pep_nterm = ''
+            pep_start = 0
+        if '-' in pep_parts[-1]:
+            pep_cterm = pep_parts[-1]
+            pep_end = -1
+        else: 
+            pep_cterm = ''
+            pep_end = 0
+
+        peptide = ''.join(pep_parts[pep_start:pep_end]) #ditch C/N modifications for truncations
+        # print("sequence:",sequence)
+        # print("peptide:",peptide)
 
         if user_frags is None:
             if 'frags' in metadf.columns:
@@ -80,49 +115,34 @@ def get_fragments(metadf,user_frags=None):
 
         for fx in frags:
             ionx = fx.split(':')[0]
-            truncx = fx.split(':')[1].split(',') if len(fx.split(':'))>1 else ['ALL']
-            chargex = (fx.split(':')[2].split(',')) if len(fx.split(':'))==3 else []
-            #print(ionx,truncx,chargex)
+            truncs = fx.split(':')[1].split(',') if len(fx.split(':'))>1 else ['ALL']
+            charges = (fx.split(':')[2].split(',')) if len(fx.split(':'))==3 else ['ALL']
+            truncx = expand_range(truncs,row.start_seq,row.end_seq)
+            max_charge = row.charge # min(len(peptide)//2,row.charge) #leave it naive for now
+            chargex = expand_range(charges,1,max_charge)
+
             if ionx in ion_frag_dict.keys():
                 trunc_type = ion_frag_dict[ionx]
             else:
                 print("ion_type not recognized")
                 continue
-            max_charge = min(len(peptide)//2,row.charge)
-            if len(chargex) > 0:
-                if 'ALL' in chargex:
-                    charges = 'ALL'
-                else: 
-                    try: charges = [int(c) for c in chargex if int(c) <= max_charge]
-                    except: 
-                        print("syntax error in charges, using ALL")
-                        charges = 'ALL'
-            else: charges = 'ALL'
-            if charges == 'ALL':
-                #max_charge = min(len(peptide)//2,row.charge)
-                charges = np.arange(1,max_charge+1)
+            
+            #lose any truncated N/C modifications
+            if trunc_type == "N-term":  
+                pep_c = ''
+            else: pep_c = pep_cterm
+            if trunc_type == "C-term":
+                pep_n = ''
+            else: pep_n = pep_nterm
 
-            for charge in charges:
-                if len(truncx) > 0:
-                    if 'ALL' in truncx:
-                        truncs = 'ALL'
-                    else: 
-                        try: truncs = [int(t) for t in truncx]
-                        except: 
-                            print("syntax error in truncations, using ALL")
-                            truncs = 'ALL'
-                #else: truncs = 'ALL'
-                if truncs == 'ALL':
-                    min_trunc = 0 #
-                    truncs = np.arange(min_trunc,len(row.peptide)-1)+1
-                #print(truncs)
-                for trunc in truncs:
+            for charge in chargex:
+                for trunc in truncx:
                     frag_df = pd.DataFrame()
                     if trunc > len(peptide): 
                         print("specified fragment longer than peptide:",trunc)
                         continue
                     
-                    sidx,eidx = get_pepfrag(trunc_type)
+                    sidx,eidx = get_pepfrag(peptide,trunc_type,trunc)
                     pep = peptide[sidx:eidx]
                     
                     frag_df = row.copy()
@@ -132,7 +152,7 @@ def get_fragments(metadf,user_frags=None):
                     frag_df['end_seq'] = row.start_seq + eidx - 1
                     frag_df['parent_range'] = frag_df['peptide_range']
                     frag_df['peptide_range'] = '-'.join([str(row.start_seq+sidx),str(row.start_seq+eidx-1)])
-                    frag_df['peptide'] = pep
+                    frag_df['peptide'] = pep_n+pep+pep_c
                     frag_df['charge'] = charge
                     frag_df['modification'] = new_mods+'ion_type:'+ionx
                     frag_df['frags']=ionx+':'+str(trunc)
